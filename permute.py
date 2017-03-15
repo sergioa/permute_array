e11a2b308da11cddb3a502e7f02acefce4421697
Sergio Artero <sergio.arteromartinez@telefonica.com>
2017-03-15 11:57:06 +0100

    remove dups

--- permute.py
+++ permute.py
@@ -7,9 +7,11 @@ def permutations(array):
     for i in range(len(array)):
         [arr.append([array[i]] + x) for x in permutations(array[0:i] + array[i+1:])]
     return arr
-    
+
+def remove_dups(array):
+    return dict((''.join(x), x) for x in array).values()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit('Usage: {0} comma_separated_list_of_elements'.format(sys.argv[0]))
-    print permutations(sys.argv[1].split(','))
+    print remove_dups(permutations(sys.argv[1].split(',')))
